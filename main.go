84335934b10ca9eec4b2113fb31ec573343fcc21
clairemuller <clairermuller@gmail.com>
2019-11-21 17:30:31 -0800

    parse links

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"flag"
-	"io"
+	"fmt"
+	"gophercises/link"
 	"log"
 	"net/http"
-	"os"
+	"net/url"
+	"strings"
 
 	"github.com/gravitational/trace"
 )
@@ -17,7 +19,30 @@ func main() {
 	resp, err := http.Get(*urlFlag)
 	check(err)
 	defer resp.Body.Close()
-	io.Copy(os.Stdout, resp.Body)
+
+	// need to add domain to paths -> /some-path
+	// need to deal with fragment or mailto links
+	reqUrl := resp.Request.URL
+	baseUrl := &url.URL{
+		Scheme: reqUrl.Scheme,
+		Host:   reqUrl.Host,
+	}
+	base := baseUrl.String()
+
+	// use the link parsing package from previous exercise
+	links, _ := link.Parse(resp.Body)
+	var hrefs []string
+	for _, ll := range links {
+		switch {
+		case strings.HasPrefix(ll.Href, "/"):
+			hrefs = append(hrefs, base+ll.Href)
+		case strings.HasPrefix(ll.Href, "http"):
+			hrefs = append(hrefs, ll.Href)
+		}
+	}
+	for _, href := range hrefs {
+		fmt.Println(href)
+	}
 }
 
 func check(err error) {
